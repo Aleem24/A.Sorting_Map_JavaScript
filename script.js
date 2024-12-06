1572eed66c2b6f609ed8eb517afb04c0ec4c8caf
Aleem24 <md.samiulaleem16@gmail.com>
2024-12-06 19:46:32 +0600

    Updated

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-let marks = [100,80,83,43,-23];
-
+//Array Sorting in 
+let marks = [100, 80, 83, 43, -23];
 function ascending(){
     marks.sort(function(a,b){return a-b});
     document.getElementById("result").innerHTML = marks;
 };
-
-
-
 function descending(){
     marks.sort(function(a,b){return b-a});
     document.getElementById("result").innerHTML = marks;
 };
-
-
-
-
-
-
+//Using Map Method
 function updatingArrayElement(num){
     return num+10;};
 
 
 let uptadedMarks = marks.map
 (updatingArrayElement);
-
 document.getElementById("map").innerHTML = uptadedMarks;
